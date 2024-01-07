0dea92699644fc1e84e8ab05dbfd94f4d15cf4c6
Lei Li <lei.xw.li@hotmail.com>
2024-01-07 14:28:05 -0800

    fixed session error

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -40,10 +40,10 @@ router.post(
       });
   
       if (!user || !bcrypt.compareSync(password, user.hashedPassword.toString())) {
-        const err = new Error('Login failed');
+        const err = new Error('Invalid credentials');
         err.status = 401;
         err.title = 'Login failed';
-        err.errors = { credential: 'The provided credentials were invalid.' };
+        // err.errors = { credential: 'The provided credentials were invalid.' };
         return next(err);
       }
   
