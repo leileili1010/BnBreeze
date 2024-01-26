7145de02030cee4000e7e5973f8a0824d3f596c7
Lei Li <lei.xw.li@hotmail.com>
2024-01-25 21:48:26 -0800

    list of spot layout completed

--- frontend/src/components/Spots/ListOfSpots.jsx
+++ frontend/src/components/Spots/ListOfSpots.jsx
@@ -2,6 +2,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
 import {thunkGetSpots} from '../../store/spots';
 import { Link } from 'react-router-dom';
+import './ListOfSpots.css'
 
 const ListOfSpots = () => {
     const dispatch = useDispatch();
@@ -15,22 +16,28 @@ const ListOfSpots = () => {
     if (!spotsArr) return null;
 
     return (
-        <div className='spots-container' >
+        <div id='spots-container' >
             {spotsArr.map(spot => (
                 <div className='spot' key={spot.id} title={spot.name}>
                     <Link key={spot.id} to={`/spots/${spot.id}`}>
+
                         <div className="preview">
                             <img className="spotImg" src={spot.previewImage} alt="preview of spot" />
                         </div>
-                        <div className='address'>{`${spot.city}, ${spot.state}`}</div>
-                        <div className='rating'>
-                            {
-                                spot.avgRating !== "No ratings yet"?
-                                (<span><i className="fa-solid fa-star"></i>{parseFloat(spot.avgRating).toFixed(1)}</span>):
-                                (<span><i className="fa-solid fa-star"></i>New</span>)
-                            }
+
+                        <div className='address-rating flex'>
+                            <div className='address'>{`${spot.city}, ${spot.state}`}</div>
+                            <div className='rating'>
+                                {
+                                    spot.avgRating !== "No ratings yet"?
+                                    (<span><i className="fa-solid fa-star"></i>{parseFloat(spot.avgRating).toFixed(1)}</span>):
+                                    (<span><i className="fa-solid fa-star"></i>New</span>)
+                                }
+                            </div>
                         </div>
-                        <div className='price'>{`$${parseFloat(spot.price).toFixed(2)} night`}</div>
+                        
+                        <span className='price'>{`$${parseFloat(spot.price).toFixed(2)}`}</span>
+                        <span className='night'>night</span>
                     </Link>
                 </div>
             ))}
