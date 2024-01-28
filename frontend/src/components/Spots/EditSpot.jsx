90acf6cac1b27b99340a80b0fb43eef6d6fa9bf7
Lei Li <lei.xw.li@hotmail.com>
2024-01-27 17:41:29 -0800

    final bug done

--- frontend/src/components/Spots/EditSpot.jsx
+++ frontend/src/components/Spots/EditSpot.jsx
@@ -3,17 +3,25 @@ import SpotForm from './SpotForm';
 import {useDispatch, useSelector} from 'react-redux';
 import { useEffect } from 'react';
 import { thunkGetSpot} from '../../store/spots';
+import { Navigate } from 'react-router-dom';
 
 const EditSpot = () => {
     const dispatch = useDispatch();
     const {spotId} = useParams();
     const spot = useSelector(state => state.spots[spotId]);
+    const userId = useSelector(state => state.session.user.id);
+    const ownerId = spot?.ownerId;
 
     useEffect(() => {
         dispatch(thunkGetSpot(spotId))
       }, [dispatch, spotId]) 
 
       if(!spot || !spot.SpotImages) return null;
+
+      if (userId !== ownerId) {
+        window.alert("Authorization is required to update this spot")
+        return <Navigate to='/' replace={true} />
+      }
     
     return (
         <>
